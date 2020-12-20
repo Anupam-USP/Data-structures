7e7a4693094bb6975287cc89cca93085df658dcc
Anupam-USP <ianupam1509@gmail.com>
2020-12-20 21:10:10 +0530

    Rec4 updated

--- Recursion/Rec4.cpp
+++ Recursion/Rec4.cpp
@@ -1,21 +1,29 @@
+//  Power of a number
 #include <iostream>
 using namespace std;
 
 int pow(int m, int n){
+	if(n==0){
+		return 1;
+	}
+	return pow(m,n-1)*m
+}
+
+int pow1(int m, int n){
  	if (n==0){
  		return 1;
  	}
  	else if(n%2==0){
- 		return pow(m*m,n/2);
+ 		return pow1(m*m,n/2);
  	}
  	else{
- 		return m*pow(m*m,(n-1)/2);
+ 		return m*pow1(m*m,(n-1)/2);
  	}
  }
 
  int main(){
  	int m,n;
  	cin>>m>>n;
- 	cout<<pow(m,n)<<endl;
+ 	cout<<pow1(m,n)<<endl;
  	return 0;
  }
