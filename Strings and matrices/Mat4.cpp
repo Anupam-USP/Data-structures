2fd6ea52a52230af56b54d0e50524208f4d47f62
Anupam-USP <ianupam1509@gmail.com>
2021-01-14 01:55:04 +0530

    D5 S&M

---
+++ Strings and matrices/Mat4.cpp
@@ -0,0 +1,56 @@
+// Sparse matrix
+#include <iostream>
+using namespace std;
+class Element{
+public:
+    int i;
+    int j;
+    int x;
+};
+
+class Sparse{
+private:
+    int m;
+    int n;
+    int num;
+    Element *ele;
+public:
+    Sparse(int m, int n, int num){
+        this->m = m;
+        this->n = n;
+        this->num = num;
+        ele = new Element[this->num];
+    }
+
+    void read(){
+        cout<<"Enter all the non-zero elements: ";
+        for(int i=0;i<num;i++)
+            cin>>ele[i].i>>ele[i].j>>ele[i].x;
+    }
+
+    void Display(){
+        int k=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(ele[k].i==i && ele[k].j==j){
+                    cout<<ele[k++].x<<" ";
+                }
+                else{
+                    cout<<"0 ";
+                }
+            }
+            cout<<endl;
+        }
+    }
+
+    ~Sparse(){
+        delete []ele;
+    }
+};
+
+int main(){
+    Sparse s1(5,5,6);
+    s1.read();
+    s1.Display();
+    return 0;
+}
